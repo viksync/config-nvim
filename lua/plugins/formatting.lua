66268b244e641b640cf3a24c2fce60be969f9176
Viktor Synkov <226089900+viksync@users.noreply.github.com>
2026-03-06 18:34:49 +0200

    +kulala, prettier config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,45 +1,28 @@
--- Prettier formatting via conform
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    default_format_opts = {
-      timeout_ms = 3000,
-      async = false,
-      quiet = false,
-      lsp_format = "fallback",
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		default_format_opts = {
+			timeout_ms = 3000,
+			async = false,
+			quiet = false,
+			lsp_format = "fallback",
+		},
 
-    formatters_by_ft = {
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescriptreact = { "prettier" },
-      json = { "prettier" },
-      jsonc = { "prettier" },
-      vue = { "prettier" },
-      css = { "prettier" },
-      scss = { "prettier" },
-      html = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-      graphql = { "prettier" },
-    },
-
-    formatters = {
-      prettier = {
-        condition = function(ctx)
-          return vim.fs.find({
-            ".prettierrc",
-            ".prettierrc.json",
-            ".prettierrc.js",
-            ".prettierrc.yaml",
-            ".prettierrc.yml",
-            "prettier.config.js",
-            "prettier.config.cjs",
-            "prettier.config.mjs",
-          }, { path = ctx.filename, upward = true })[1]
-        end,
-      },
-    },
-  },
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			vue = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			html = { "prettier" },
+			yaml = { "prettier" },
+			yml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+		},
+	},
 }
---
+++ lua/plugins/kulala.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"mistweaverco/kulala.nvim",
+		opts = {
+			ui = {
+				show_request_summary = false,
+			},
+		},
+	},
+}
